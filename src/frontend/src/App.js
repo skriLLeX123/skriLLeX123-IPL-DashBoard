a94ca463ead299905968027424cdb0f2b14ccdd9
Bhushan <bhushan.navgire@gmail.com>
2021-08-23 16:00:10 -0400

    Added Team Page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import {TeamPage} from './pages/TeamPage';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-     <TeamPage/>
+      <Router>
+        <Route path="/teams/:teamName">
+          <TeamPage/>
+        </Route>
+      </Router>
+     
     </div>
   );
 }
